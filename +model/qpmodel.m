04eb56154221618393c65bdae4ee32282bb37aed
Dominique Orban <dominique.orban@gmail.com>
2018-03-12 10:34:16 +0100

    remove whitespace

--- +model/qpmodel.m
+++ +model/qpmodel.m
@@ -1,49 +1,49 @@
 classdef qpmodel < model.nlpmodel
-    % nlpmodel where 
+    % nlpmodel where
     %   objectif is quadratic     : f(x) = c' * x + 1/2 x' * Q * x
     %   constraints are linear    : c(x) = A  * x
-    
+
    properties (SetAccess = private, Hidden = false)
       A    % Jacobian of linear constraints
       c    % Gradient of linear objectif
       Q    % Hessien (symmetric)
    end
-    
+
     methods (Sealed = true)
-        
+
         function o = qpmodel(name, x0, cL, cU, bL, bU, A, c, Q)
-            
+
             o = o@model.nlpmodel(name, x0, cL, cU, bL, bU);
-            
+
             % Store other things.
             o.A = A;
             o.c = c;
             o.Q = Q;
-            
+
             % In lpmodel, all constraints are linear
             o.linear = true(o.m, 1);
         end
-        
+
         function f = fobj_local(self, x)
             f = self.c' * x + 0.5 * x' * self.Q * x;
         end
-        
+
         function g = gobj_local(self, x)
             g = self.c + self.Q * x;
         end
-        
+
         function H = hobj_local(self, ~)
             H = self.Q;
         end
-        
+
         function c = fcon_local(self, x)
             c = self.A * x;
         end
-        
+
         function J = gcon_local(self, ~)
             J = self.A;
         end
-        
+
         function Hc = hcon_local(self, ~, ~)
             Hc = sparse(self.n, self.n);
         end
@@ -51,15 +51,15 @@ classdef qpmodel < model.nlpmodel
         function H = hlag_local(self, varargin)
             H = sparse(self.n, self.n);
         end
-        
+
         function w = hlagprod_local(self, ~, ~, ~)
             w = sparse(self.n, 1);
         end
-        
+
         function w = hconprod_local(self, ~, ~, ~)
             w = sparse(self.n, 1);
         end
-        
+
         function w = ghivprod_local(self, ~, ~, ~)
             w = sparse(self.n, 1);
         end
