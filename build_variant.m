b1387129c16c1e561793c0ed993e2b278f2d68d3
Dominique Orban <dominique.orban@gmail.com>
2018-03-14 10:01:31 +0100

    record pdcoo home

--- build_variant.m
+++ build_variant.m
@@ -1,10 +1,10 @@
-function classname = build_variant(formulation, solver)
+function classname = build_variant(pdcoo_home, formulation, solver)
 
   classname = sprintf('pdco_%s_%s', formulation, solver);
-  fname = ['Variants/' , classname, '.m'];
-  if exist('Variants','dir')~= 7
-    % Variants directory does not exist, make it.
-    mkdir('Variants');
+  variants_dir = fullfile(pdcoo_home, 'Variants')
+  fname = fullfile(variants_dir, sprintf('%s.m', classname));
+  if exist(variants_dir, 'dir') ~= 7
+    mkdir(variants_dir);
   end
   fid = fopen(fname, 'w');
   code = format_template(formulation, solver);
--- setup.m
+++ setup.m
@@ -1,6 +1,12 @@
-addpath(fullfile(pwd, 'Formulations'))
-addpath(fullfile(pwd, 'Solvers'))
-addpath(pwd)
-addpath(fullfile(pwd, 'readmps'))
-addpath(fullfile(pwd, 'Variants'))
-addpath(fullfile(pwd, 'Tools'))
+pdcoo_home = pwd;
+
+addpath(pdcoo_home)
+addpath(fullfile(pdcoo_home, 'Formulations'))
+addpath(fullfile(pdcoo_home, 'Solvers'))
+addpath(fullfile(pdcoo_home, 'readmps'))
+addpath(fullfile(pdcoo_home, 'Tools'))
+if exist('Variants', 'dir') ~= 7
+  mkdir('Variants');
+end
+addpath(fullfile(pdcoo_home, 'Variants'))
+addpath(fullfile(pdcoo_home, 'Test'))
